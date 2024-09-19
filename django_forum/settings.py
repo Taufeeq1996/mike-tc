6ed85f0798cba509df552b47a0746abb09fdccbe
Taufeeq1996 <taufeeqk@techis.io>
2024-09-19 07:56:43 +0530

    done

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -111,12 +111,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
 # print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
